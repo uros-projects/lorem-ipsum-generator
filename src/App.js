6131cf74fae8c3cc9c61d0aa067b62e10c062c28
Uros <uros.tomasevic.work@gmail.com>
2021-01-18 00:13:53 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import data from './data';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [count, setCount] = useState(0)
+	const [text, setText] = useState([data[0]])
+	const [maxNumber, setMaxNumber] = useState(data.length)
+
+	const handleSubmit = e => {
+		e.preventDefault();
+
+		let pNumber = parseInt(count);
+		if(count <= 0) {
+			pNumber = 1;
+		}
+		if(count > maxNumber) {
+			pNumber = maxNumber;
+		}
+
+		setText(data.slice(0, pNumber));
+	}
+
+	return (
+		<main>
+			<section className='section-center'>
+				<h3>tired of boring lorem ipsum?</h3>
+				<form className='lorem-form' onSubmit={handleSubmit}>
+					<label htmlFor='number'>
+						paragraphs:
+					</label>
+					<input
+						type='number'
+						name='number'
+						id='number'
+						value={count}
+						onChange={ e => setCount(e.target.value) } />
+						<button type='submit' className='btn'>generate</button>
+				</form>
+				<article className='lorem-text'>
+					{text.map( (item, index) => {
+						return <p key={index}>{item}</p>
+					})}
+				</article>
+			</section>
+		</main>
+	);
 }
 
 export default App;
